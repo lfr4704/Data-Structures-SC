cd29a03198579269d97fac878cbaa5f7821c0253
Luis Robles <lfr4704@gmail.com>
2020-09-28 00:28:06 -0700

    ring buffer

--- Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
+++ Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
@@ -1,9 +1,31 @@
+from collections import deque
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = deque(maxlength=self.capacity)
+        self.neg_count = 1
+        self.pos_count = 0
+
 
     def append(self, item):
-        pass
+        if self.capacity == (self.neg_count -1):
+            self.neg_count = 1
+            self.pos_count = 0
+
+        #if ring buffer is not full
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+
+        #ring buffer is full
+        elif self.capacity == len(self.storage):
+            self.storage.rotate(self.capacity - self.neg_count)
+            self.storage.append(item)
+            self.storage.rotate(self.capacity + self.pos_count)
+            self.neg_count = self.neg_count + 1
+            self.pos_count = self.pos_count + 1
+
 
     def get(self):
-        pass
+        return list(self.storage)
