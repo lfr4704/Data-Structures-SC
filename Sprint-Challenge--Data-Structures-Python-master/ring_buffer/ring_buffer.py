18fb493fe12230a8e9b575d0ba16fb47d816c459
Luis Robles <lfr4704@gmail.com>
2020-09-28 19:50:09 -0700

    added progress

--- Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
+++ Sprint-Challenge--Data-Structures-Python-master/ring_buffer/ring_buffer.py
@@ -4,13 +4,13 @@ from collections import deque
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = deque(maxlength=self.capacity)
+        self.storage = deque(maxlen=self.capacity)
         self.neg_count = 1
         self.pos_count = 0
 
 
     def append(self, item):
-        if self.capacity == (self.neg_count -1):
+        if self.capacity == (self.neg_count - 1):
             self.neg_count = 1
             self.pos_count = 0
 
@@ -29,3 +29,11 @@ class RingBuffer:
 
     def get(self):
         return list(self.storage)
+
+if __name__ == '__main__':
+    x = RingBuffer(5)
+    x.append(1)
+    x.append(2)
+    x.append(3)
+    x.append(4)
+    print(x.get())
